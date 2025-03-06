e80377c4ae418462ff0d92dff06c88d211a000b6
khushmnsangha <anmolsangha02@gmail.com>
2025-03-06 18:36:26 +0530

    Add Category model schema

---
+++ backend/models/Category.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const CategorySchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    recipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }],
+});
+
+module.exports = mongoose.model('Category', CategorySchema);
