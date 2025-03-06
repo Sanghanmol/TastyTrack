2fdc006a7341402d0e90c2e5008098cb506c585b
khushmnsangha <anmolsangha02@gmail.com>
2025-03-06 18:38:59 +0530

    Initialize Express server and API routes

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const logger = require('./middleware/logger');
+
+require('dotenv').config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(logger);
+
+app.use('/api/recipes', require('./routes/recipes'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
