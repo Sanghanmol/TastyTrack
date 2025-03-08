62893ba5a79a598e4641319cea0885e40321dfa9
khushmnsangha <anmolsangha02@gmail.com>
2025-03-08 20:02:10 +0530

    setup Express server with recipe routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const logger = require('./middleware/logger');
+const recipeRoutes = require("./routes/recipes");
 
 require('dotenv').config();
 connectDB();
@@ -11,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use(logger);
 
-app.use('/api/recipes', require('./routes/recipes'));
+app.use("/api/recipes", recipeRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
