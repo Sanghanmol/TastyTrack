b3bcde147c2d39f384eb8f43bf463802e21d3a5a
khushmnsangha <anmolsangha02@gmail.com>
2025-03-08 20:06:37 +0530

    create recipe page to manage and display recipe list

---
+++ frontend/src/pages/RecipePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RecipeDetails from "../components/RecipeDetails";
+
+const RecipePage = () => {
+  return (
+    <div>
+      <RecipeDetails />
+    </div>
+  );
+};
+
+export default RecipePage;
