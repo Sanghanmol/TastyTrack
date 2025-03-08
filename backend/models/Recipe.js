7504383709d54038dd261233620e789e2d220fb4
khushmnsangha <anmolsangha02@gmail.com>
2025-03-08 20:01:23 +0530

    Updated Recipe schema

--- backend/models/Recipe.js
+++ backend/models/Recipe.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 
 const RecipeSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    description: { type: String, required: true },
     ingredients: { type: [String], required: true },
     instructions: { type: String, required: true },
-    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
+    category: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Recipe', RecipeSchema);
