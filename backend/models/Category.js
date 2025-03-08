07885e0f0d814b0646c26901deece308efdec889
khushmnsangha <anmolsangha02@gmail.com>
2025-03-08 19:59:46 +0530

    Removed unnecesaary files

--- backend/models/Category.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const CategorySchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    recipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }],
-});
-
-module.exports = mongoose.model('Category', CategorySchema);
